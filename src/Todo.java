85300c6c43b37974b4af869899ed3a77021c1695
TravidB <travisbu12345@gmail.com>
2025-05-20 22:06:59 -0700

    check method

--- src/Todo.java
+++ src/Todo.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Todo {
@@ -15,9 +16,35 @@ public class Todo {
         System.out.println("-1. to end the application");
     }
 
-    public void check(boolean x){
-        //
+    public static ArrayList<HashMap<String, Integer>> removeHash(ArrayList<HashMap<String, Integer>> list2, String remove){
+        Iterator<HashMap<String, Integer>> it = list2.iterator();
+        int index = 0;
+
+        while(it.hasNext()){
+            HashMap<String, Integer> item = it.next();
+            for(String k : item.keySet()){
+                if(k.equals(remove)){
+                    list2.remove(index);
+                } else {
+                    index++;
+                    continue;
+                }
+            }
+        }
+
+        return list2;
     }
+
+    public static String check(int c){
+        if(c == 1){
+            return "checked";
+        } else if (c == -1){
+            return "unchecked";
+        } else {
+            return "invalid";
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         
         choices();
@@ -38,13 +65,14 @@ public class Todo {
                 String removedItem = in.next();
                 System.out.println("removed " + list.remove(removedItem));
             } else if (input == 3){
-                for(HashMap<String, Integer>item : list){
-                    
-                    for(String k : item.keySet()){
-                        System.out.println(k + " | " + item.get(k));
-                    }
-                    //System.out.println(key + " | " + item.get(key));
-                }
+                
+                System.out.println("Input name of checked item: ");
+                String checker = in.next();
+                list = removeHash(list, checker);
+                HashMap<String, Integer> item = new HashMap<>();
+                item.put(checker, 1);
+                list.add(item);
+                
             }
             choices();
             input = in.nextInt();
@@ -54,7 +82,7 @@ public class Todo {
                 for(HashMap<String, Integer>item : list){
                     
                     for(String k : item.keySet()){
-                        System.out.println(k + " | " + item.get(k));
+                        System.out.println(k + " | " + check(item.get(k)));
                     }
                     //System.out.println(key + " | " + item.get(key));
                 }
